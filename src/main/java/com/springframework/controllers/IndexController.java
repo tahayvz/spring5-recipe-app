b9c4478833db7d9a941333a1e500a01360991edf
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2020-07-12 10:01:12 +0300

    adding Project lombok and excluding properties for hash

--- src/main/java/com/springframework/controllers/IndexController.java
+++ src/main/java/com/springframework/controllers/IndexController.java
@@ -1,11 +1,13 @@
 package com.springframework.controllers;
 
 import com.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -17,6 +19,8 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
+
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
--- src/main/java/com/springframework/services/RecipeServiceImpl.java
+++ src/main/java/com/springframework/services/RecipeServiceImpl.java
@@ -3,11 +3,14 @@ package com.springframework.services;
 
 import com.springframework.domain.Recipe;
 import com.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +22,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
