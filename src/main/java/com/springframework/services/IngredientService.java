23c37b8b72acf2c63d382d88c5499199c84a7b02
abc <abc@example.com>
2020-08-28 08:53:58 +0300

    adding delete ingredient

--- src/main/java/com/springframework/services/IngredientService.java
+++ src/main/java/com/springframework/services/IngredientService.java
@@ -7,4 +7,7 @@ public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
 
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
+
 }
