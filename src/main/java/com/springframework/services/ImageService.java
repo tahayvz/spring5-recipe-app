0fca156db11aca3b8497071603379106438dd7da
abc <abc@example.com>
2020-08-28 09:21:23 +0300

    adding image upload - front end

---
+++ src/main/java/com/springframework/services/ImageService.java
@@ -0,0 +1,8 @@
+package com.springframework.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
