6c1fc9df77f026e67d07f02179cf57e4ce3bb0bb
abc <abc@example.com>
2020-08-28 11:12:34 +0300

    adding data validation

--- src/main/java/com/springframework/commands/RecipeCommand.java
+++ src/main/java/com/springframework/commands/RecipeCommand.java
@@ -1,11 +1,15 @@
 package com.springframework.commands;
 
-
 import com.springframework.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,13 +18,30 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
