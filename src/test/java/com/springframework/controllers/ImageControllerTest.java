b40995cc80eeee606e750bbcb6810764e9ab6e84
abc <abc@example.com>
2020-08-28 10:08:07 +0300

    Refactoring for Controller-advice

--- src/test/java/com/springframework/controllers/ImageControllerTest.java
+++ src/test/java/com/springframework/controllers/ImageControllerTest.java
@@ -35,7 +35,9 @@ public class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler())
+                .build();
     }
 
     @Test
@@ -98,5 +100,11 @@ public class ImageControllerTest {
         assertEquals(s.getBytes().length, reponseBytes.length);
     }
 
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
 
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 } 
